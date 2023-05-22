b43c0073b76c82f281b563f6dfb20b5b2b22bbef
Sawyer Czupka <sczupka@email.wm.edu>
2023-05-21 22:39:11 -0400

    Initial

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="bg-stone-200 h-screen w-screen">
+    <p class="pt-36 text-9xl text-center font-extrabold font-sans">
+      Hey, im <span class="text-red-600">Sawyer</span>
+    </p>
+    <div class="pt-12 text-lg flex w-2/4 m-auto">
+      <p class="text-center">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id pariatur
+        necessitatibus, itaque quod officiis tenetur aut recusandae aliquam sed
+        nemo eos facilis veniam labore accusantium ullam. Ipsa quia nobis
+        necessitatibus.
+      </p>
+    </div>
+
+    <div class="pt-48 flex  text-xl font-bold">
+    <p>Linkedin</p>
+    <p>Resume</p>
+    <p>GitHub</p>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/google-fonts'
+    ],
+    googleFonts: {
+        families: {
+          'Open+Sans': true,
+          'Josefin+Sans': true,
+          Lato: [100, 300],
+          Raleway: {
+            wght: [100, 400],
+            ital: [100]
+          },
+        }
+      }
+})
